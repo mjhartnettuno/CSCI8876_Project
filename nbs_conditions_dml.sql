6ec3645522b0c8f9273b8eba090a6d707ba298ba
mjhartnettuno <127349599+mjhartnettuno@users.noreply.github.com>
2024-05-05 10:28:31 -0500

    Update nbs_conditions_dml.sql

--- nbs_conditions_dml.sql
+++ nbs_conditions_dml.sql
@@ -1,1 +1,125 @@
+-- Use my database in mysql
+USE mhartnett;
 
+-- DML Statements
+
+INSERT INTO nbsConditions VALUES (
+  "2,4 Dienoyl_CoA reductase deficiency", 
+  "DE-RED",
+  "Fatty Acid Oxidation Disorder",
+  "NADK2",
+  "Autosomal recessive",
+  "1 case detected",
+  "Dietary Considerations, L-Carnitine Supplements",
+  "NE"
+  );
+
+INSERT INTO nbsConditions VALUES (
+  "2-methylbutyrylglycinuria", 
+  "2MBG",
+  "Organic Acid Condition",
+  "ACADSB",
+  "Autosomal recessive",
+  "1:250-500 with Hmong ancestry",
+  "Dietary Considerations, L-Carnitine Supplements",
+  "IA, NE, SD"
+  );
+
+INSERT INTO nbsConditions VALUES (
+  "3-hydroxy-3-methylglutaric Aciduria", 
+  "HMG",
+  "Organic Acid Condition",
+  "HMGCL",
+  "Autosomal recessive",
+  "< 1:100,000",
+  "Dietary Considerations, L-Carnitine Supplements",
+  "CO, IA, KS, NE, SD"
+  );
+
+INSERT INTO nbsConditions VALUES (
+  "3-Methylglutaconic Aciduria", 
+  "3MGA",
+  "Organic Acid Condition",
+  "AUH, TAZ, OPA3, DNAJC19",
+  "Autosomal recessive",
+  "Very Rare",
+  "Cardiac Monitoring",
+  "CO, IA, NE, SD"
+  );
+
+INSERT INTO nbsConditions VALUES (
+  "3-Methylcrotonyl-CoA Carboxylase Deficiency", 
+  "3-MCC",
+  "Organic Acid Condition",
+  "MCCC1, MCCC2",
+  "Autosomal recessive",
+  "< 120 babies per year",
+  "Dietary Considerations, L-Carnitine Supplements",
+  "CO, IA, KS, NE, SD"
+  );
+
+INSERT INTO nbsConditions VALUES (
+  "Argininemia", 
+  "ARG",
+  "Amino Acid Disorder",
+  "ARG1",
+  "Autosomal recessive",
+  "< 15 babies per year",
+  "Dietary Considerations",
+  "CO, IA, NE, SD"
+  );
+
+INSERT INTO nbsConditions VALUES (
+  "Argininosuccinic Aciduria", 
+  "ASA",
+  "Amino Acid Disorder",
+  "ASL",
+  "Autosomal recessive",
+  "1:70,000",
+  "Dietary Considerations, Arginine Supplements",
+  "CO, IA, KS, NE, SD"
+  );
+
+INSERT INTO nbsConditions VALUES (
+  "Benign Hyperphenylalaninemia", 
+  "H-PHE",
+  "Amino Acid Disorder",
+  "PAH",
+  "Autosomal recessive",
+  "15-75:1,000,000",
+  "Dietary Considerations for extreme cases",
+  "IA, NE, SD"
+  );
+
+INSERT INTO nbsConditions VALUES (
+  "Beta-Ketothiolase Deficiency", 
+  "BKT",
+  "Organic Acid Condition",
+  "ACAT1",
+  "Autosomal recessive",
+  "< 1:1,000,000",
+  "Dietary Considerations, L-Carnitine Supplements, Bicitra",
+  "CO, IA, KS, NE, SD"
+  );
+
+INSERT INTO nbsConditions VALUES (
+  "S, Beta-Thalassemia", 
+  "HB S/BTh",
+  "Hemoglobin Disorder",
+  "HBB",
+  "Autosomal recessive",
+  "1,000s born per year",
+  "Fluids, Pain Medication, Antibiotics, Hydroxyurea, Blood Transfusions",
+  "CO, IA, KS, NE, SD"
+  );
+
+INSERT INTO nbsConditions VALUES (
+  "S, Beta-Thalassemia", 
+  "HB S/BTh",
+  "Hemoglobin Disorder",
+  "HBB",
+  "Autosomal recessive",
+  "1,000s born per year",
+  "Fluids, Pain Medication, Antibiotics, Hydroxyurea, Blood Transfusions",
+  "CO, IA, KS, NE, SD"
+  );
