10f36eb1627c1171cdf011d024c4340003059ad4
mjhartnettuno <127349599+mjhartnettuno@users.noreply.github.com>
2024-05-05 09:27:06 -0500

    Update project_DDL.sql

--- project_DDL.sql
+++ project_DDL.sql
@@ -15,17 +15,3 @@ CREATE TABLE nbsConditions (
   incidence VARCHAR(15),
   PRIMARY KEY (condition_name)
 );
-
-CREATE TABLE treatments (
-  treatment_name VARCHAR(50),
-  condition_name VARCHAR(50),
-  treatment_type VARCHAR(30),
-  trial_progress VARCHAR(30),
-  PRIMARY KEY (treatment_name)
-);
-
-CREATE TABLE pilots (
-  st_abbrev CHAR(2),
-  condition_name VARCHAR(50),
-  PRIMARY KEY (st_abbrev,condition_name)
-);
